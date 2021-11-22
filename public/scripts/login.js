4fcdd7880c289bb57127e4737fcdc109cf195c2f
Admin <Dan123456dz@gmail.com>
2021-11-22 22:25:09 +0700

    sửa tí giao diện

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,2 +1,7 @@
 const userInput = document.querySelectorAll('input');
+const logo = document.querySelector('div.head__logo');
+const gobackbtn = document.querySelector('btn.login-form-goback');
+gobackbtn.firstElementChild.setAttribute('href', '/');
+logo.firstElementChild.setAttribute('href', '/');
+
 
