b5667208124e3e257da90da1efa11bcdf3a43955
Nhat Anh <54491115+ZNhatAnhZ@users.noreply.github.com>
2021-12-13 17:29:00 +0700

    cleaning things up

--- src/controllers/recommender.py
+++ src/controllers/recommender.py
@@ -116,49 +116,8 @@ try:
     data = rs.recommend(int(sys.argv[2]))
     print(data)
     sys.stdout.flush()
-
-    # jsondata = json.loads(sys.argv[1])
-    # arrayOfReviews = np.asarray(jsondata, dtype=np.float)
-    # print(arrayOfReviews)
-    # sys.stdout.flush()
 except Exception as e:
     # print(traceback.format_exc())
     sys.stdout.flush()
 
 
-# sample = [
-#     [0, 0, 5.0],
-#     [0, 1, 4.0],
-#     [0, 3, 2.0],
-#     [0, 4, 2.0],
-#     [1, 0, 5.0],
-#     [1, 2, 4.0],
-#     [1, 3, 2.0],
-#     [1, 4, 0.0],
-#     [2, 0, 2.0],
-#     [2, 2, 1.0],
-#     [2, 3, 3.0],
-#     [2, 4, 4.0],
-#     [3, 0, 0.0],
-#     [3, 1, 0.0],
-#     [3, 3, 4.0],
-#     [4, 0, 1.0],
-#     [4, 3, 4.0],
-#     [5, 1, 2.0],
-#     [5, 2, 1.0],
-#     [6, 2, 1.0],
-#     [6, 3, 4.0],
-#     [6, 4, 5.0],
-# ]
-# rs = Recommendation_System(np.array(sample), 3)
-# print(rs.recommend(7))
-
-# sample1 = [
-#     [8.0, 2.0, 5.0],
-#     [22.0, 1.0, 5.0],
-#     [23.0, 3.0, 5.0],
-#     [24.0, 0.0, 5.0],
-# ]
-
-# rs = Recommendation_System(np.array(sample1), 2)
-# print(rs.recommend(26))
